36d0ae6c2ce9160a4a9908771217d273680216d5
Denise Chan <waeyee.chan@gmail.com>
2024-02-22 20:01:45 +0000

    Trying to implement global score and failing lol

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,22 @@
 # pylint: disable=missing-docstring
 
 from flask import Flask, render_template, request
+from flask_session import Session
 from longest_word.game import Game
 
 app = Flask(__name__)
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Yohann's code
+# SESSION_TYPE = 'redis'
+# app.config.from_object(__name__)
+# Session(app)
 
 @app.route('/')
 def home():
     game = Game()
-    return render_template('home.html', grid=game.grid)
+    return render_template('home.html', grid=game.grid, score = 0)
 
 
 
@@ -18,4 +26,6 @@ def check():
     game.grid = list(request.form['grid'])
     word = request.form['word']
     is_valid = game.is_valid(word)
-    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+    if is_valid:
+        score += 1
+    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word, score = score)
